4061a81d0d1cb9b5adc228c5ab0873b5dfdd201b
Mitya <partyzan327@gmail.com>
2023-02-19 11:42:06 +0300

    feat: add layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { CardsListComponent } from './components/main/cards-list/cards-list.component';
+import { CardsComponent } from './components/main/cards/cards.component';
+import { SearchComponent } from './components/header/search/search.component';
+import { FilterComponent } from './components/header/filter/filter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    CardsListComponent,
+    CardsComponent,
+    SearchComponent,
+    FilterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
