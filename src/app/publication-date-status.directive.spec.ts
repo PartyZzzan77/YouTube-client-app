884bf9bf67cff747c9f4a47a58265c9c45c22b49
Mitya <partyzan327@gmail.com>
2023-02-20 22:52:51 +0300

    feat: date directive

---
+++ src/app/publication-date-status.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PublicationDateStatusDirective } from './publication-date-status.directive';
+
+describe('PublicationDateStatusDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PublicationDateStatusDirective();
+    expect(directive).toBeTruthy();
+  });
+});
