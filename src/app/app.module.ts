7b71f4d1fe5de2bea09e7f126cf79bc0a51678f6
Mitya <partyzan327@gmail.com>
2023-02-20 14:36:23 +0300

    feat: add header items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { CardComponent } from './components/main/card/card.component';
 import { SearchComponent } from './components/header/search/search.component';
 import { FilterComponent } from './components/header/filter/filter.component';
 import { AccountComponent } from './components/header/account/account.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -22,7 +28,16 @@ import { AccountComponent } from './components/header/account/account.component'
     FilterComponent,
     AccountComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatTooltipModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/header/search/search.component.ts
+++ src/app/components/header/search/search.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {}
+export class SearchComponent {
+  value = '';
+}
