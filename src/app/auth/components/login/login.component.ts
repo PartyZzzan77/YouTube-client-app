7c00f2cf8ce0249120675eaab31e105a1ff54e1c
Mitya <partyzan327@gmail.com>
2023-03-10 12:53:35 +0300

    feat: add validation for login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,17 +7,30 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   public SubmitLabel = 'Submit';
+
+  public isInvalid = '';
   private loginField: string;
   private passwordField: string;
 
-  setLogin(value: string) {
+  private checkAuth = () => {
+    if (!this.loginField || !this.passwordField) {
+      this.isInvalid = 'required fields login & password';
+    } else {
+      this.isInvalid = '';
+    }
+  };
+
+  public setLogin(value: string) {
     this.loginField = value;
   }
-  setPassword(value: string) {
+  public setPassword(value: string) {
     this.passwordField = value;
   }
 
   handleSubmit() {
+    this.checkAuth();
+    if (this.isInvalid) return;
+
     const user = JSON.stringify({
       login: this.loginField,
       password: this.passwordField,
